e9f396fac2bc36192e3ad618f832d3c5692dc450
Ninaiskov <67420369+Ninaiskov@users.noreply.github.com>
2023-11-30 21:04:27 +0100

    Update run_syn_batchjobs.sh

--- run_syn_batchjobs.sh
+++ run_syn_batchjobs.sh
@@ -7,7 +7,7 @@ do
 for alpha in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.98
 do
     sed -i '$ d' submit_hpc.sh
-    echo "python3 main.py --dataset synthetic_article --maxiter_gibbs 100 --K $K --Nc_type $Nc_type --alpha $alpha --model_type parametric --noc 10" >> submit_hpc.sh
+    echo "python3 main.py --dataset synthetic --maxiter_gibbs 100 --K $K --Nc_type $Nc_type --alpha $alpha --model_type parametric --noc 10" >> submit_hpc.sh
     bsub < submit_hpc.sh
 done
 done
